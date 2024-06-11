774c7d36f532cca4724dbce1eb90969c5a963c96
Evgeniy Pashko <evgpashko92@gmail.com>
2024-06-12 02:14:05 +0700

    refactor db configuration

---
+++ config/db/mysql.ts
@@ -0,0 +1,6 @@
+export const MYSQL_CONFIG = {
+  host: process.env.NUXT_SERVER_MYSQL_HOST,
+  user: process.env.NUXT_SERVER_MYSQL_USER,
+  password: process.env.NUXT_SERVER_MYSQL_PASSWORD,
+  database: process.env.NUXT_SERVER_MYSQL_DATABASE,
+};
--- server/api/posts.ts
+++ server/api/posts.ts
@@ -1,17 +1,16 @@
 import mysql from "mysql2/promise";
 
 export default defineEventHandler(async (event) => {
-    // const config = useRuntimeConfig(event);
-    // console.log(process.env);
-    // console.log(config);
+  const config = useRuntimeConfig(event);
+  //console.log(config);
 
-    const connection = await mysql.createConnection({
-        host: process.env.NUXT_SERVER_MYSQL_HOST,
-        user: process.env.NUXT_SERVER_MYSQL_USER,
-        password: process.env.NUXT_SERVER_MYSQL_PASSWORD,
-        database: process.env.NUXT_SERVER_MYSQL_DATABASE,
-    });
+  const connection = await mysql.createConnection({
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+  });
 
-    const [rows] = await connection.execute("SELECT * FROM posts");
-    return rows;
+  const [rows] = await connection.execute("SELECT * FROM posts");
+  return rows;
 });
