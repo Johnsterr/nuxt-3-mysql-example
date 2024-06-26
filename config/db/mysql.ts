39fd23ea0a9d22611395a6334a392b548193e0d1
Evgeniy Pashko <evgpashko92@gmail.com>
2024-06-26 22:49:55 +0700

    setting pools for sequelize

--- config/db/mysql.ts
+++ config/db/mysql.ts
@@ -14,6 +14,12 @@ const sequelize = new Sequelize(
   {
     host: MYSQL_CONFIG.host,
     dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   }
 );
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,21 @@
 <template>
-    <div>
-        <h1>Posts</h1>
-        <ul>
-            <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
-        </ul>
-    </div>
+  <div>
+    <h1>Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import {ref, onMounted} from "vue";
 
 const posts = ref([]);
 
 onMounted(async () => {
-    const response = await fetch("/api/posts");
-    posts.value = await response.json();
+  const response = await fetch("/api/posts");
+  posts.value = await response.json();
 });
 </script>
